6ca619968406cacb3fbd402aa5dc4db10907388f
Minindu Hewawasam <w1698509@my.westminster.ac.uk>
2019-04-29 16:33:53 +0530

    Update questionSelector.py

--- SmartGuruAppServerside/questionSelector.py
+++ SmartGuruAppServerside/questionSelector.py
@@ -12,9 +12,9 @@ metadata = pd.read_sql("select * from questions", conn)
 
 # metadata = pd.read_csv('quizes.csv')
 
-print(metadata.head(2))
+# print(metadata.head(2))
 
-print("new commit")
+# print("new commit")
 
 features = ['question', 'chapter', 'topic']
 
@@ -63,28 +63,23 @@ metadata = metadata.reset_index()
 indices = pd.Series(metadata.index, index=metadata['question'])
 
 
-def get_recommendations(idno, cosine_sim=cosine_sim2):
-    idx = indices[idno]
-
+def get_recommendations(questionID, cosine_sim=cosine_sim2):
+    idx = indices[questionID]
     sim_scores = list(enumerate(cosine_sim[idx]))
-
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-
-    sim_scores = sim_scores[0:5]
-
+    sim_scores = sim_scores[1:6]
     question_indices = [i[0] for i in sim_scores]
-
-    return metadata['idno'].iloc[question_indices]
-
-
-def main(idNum):
-    print(get_recommendations(idNum, cosine_sim2))
+    return question_indices
 
 
-if __name__ == '__main__':
-
-    while(True):
-        idnumber = int(input("enter question id"))
-        print(idnumber)
-
-        main(idnumber)
+# def main(idNum):
+#     print(get_recommendations(idNum, cosine_sim2))
+#
+#
+# if __name__ == '__main__':
+#
+#     while(True):
+#         idnumber = int(input("enter question id"))
+#         # print(idnumber)
+#
+#         main(idnumber)
