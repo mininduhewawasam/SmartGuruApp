d96333615181927de51aadf611373fb236e85404
Minindu Hewawasam <w1698509@my.westminster.ac.uk>
2019-04-30 22:03:32 +0530

    Update recQuizGenerator.py

--- SmartGuruAppServerside/recQuizGenerator.py
+++ SmartGuruAppServerside/recQuizGenerator.py
@@ -10,6 +10,8 @@ sentjson = json
 
 
 def sendQuestions():
+    recQuizList2.clear()
+    recQuizList.clear()
     try:
 
         mySQLconnection = mysql.connector.connect(host='www.remotemysql.com',
@@ -17,11 +19,17 @@ def sendQuestions():
                                                   user='u2oI1tyJuT',
                                                   password='joBxFoudcl')
 
+        # mySQLconnection = mysql.connector.connect(host='Localhost',
+        #                                           database='smartgurunew',
+        #                                           user='root',
+        #                                           password='')
+
         sql_select_Query = "select QuestionID from sessionsdata where userID=1"
         cursor1 = mySQLconnection.cursor()
         cursor1.execute(sql_select_Query)
         records = cursor1.fetchall()
-        
+        # print(records)
+
         for row in records:
             sql_select_Query2 = "select * from questions where questionID=" + str(
                 questionSelector.get_recommendations(row[0])[0])+""
@@ -29,8 +37,10 @@ def sendQuestions():
             cursor2.execute(sql_select_Query2)
             records2 = cursor2.fetchall()
             recQuizList.append(records2)
+            # print(records2)
 
         for row2 in recQuizList:
+            # print(row2[0][0])
             answerList = row2[0][7].split(',')
 
             recQuizList2.append({'qs_id': row2[0][0], 'qs_topic': row2[0][9], 'question': row2[0][1], 'options': {'op1': row2[0][2], 'op2': row2[0][3], 'op3': row2[0][4], 'op4': row2[0][5], 'op5': row2[0][6]}, 'answers': answerList,   'difficulty': row2[0][10]})
@@ -39,7 +49,6 @@ def sendQuestions():
         print("Error while connecting to MySQL", e)
 
     return json.dumps(recQuizList2)
-
 
 
 
