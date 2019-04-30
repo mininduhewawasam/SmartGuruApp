f4e5d1af8d947ba2d4f526c7f4f14500693c36fd
Minindu Hewawasam <w1698509@my.westminster.ac.uk>
2019-04-30 12:39:22 +0530

    Update recQuizGenerator.py

--- SmartGuruAppServerside/recQuizGenerator.py
+++ SmartGuruAppServerside/recQuizGenerator.py
@@ -7,42 +7,10 @@ from mysql.connector import Error
 recQuizList=[]
 recQuizList2=[]
 sentjson=json
-try:
-    mySQLconnection = mysql.connector.connect(host='localhost',
-                                              database='smartguru',
-                                              user='root',
-                                              password='')
-
-    sql_select_Query = "select QuestionID,sessionID from sessionsdata where userID=2"
-    cursor = mySQLconnection.cursor()
-    cursor.execute(sql_select_Query)
-    records = cursor.fetchall()
-
-    # sql_select_Query2 = "select * from questions where questionID=1"
-    # cursor2 = mySQLconnection.cursor()
-    # cursor2.execute(sql_select_Query2)
-    # records2 = cursor.fetchall()
-
-    # print("SELECT questions.question FROM questions INNER JOIN sessionsdata on sessionsdata.QuestionID=questions.questionID WHERE sessionsdata.userID=2;")
-
-    for row in records:
-        # print(questionSelector.get_recommendations(row[0])[0])
-        #
-        # print("-------------------")
 
 
 
-        sql_select_Query2 = "select * from questions where questionID="+str(questionSelector.get_recommendations(row[0])[0])+""
-        cursor2 = mySQLconnection.cursor()
-        cursor2.execute(sql_select_Query2)
-        records2 = cursor2.fetchall()
 
-        recQuizList.append(records2)
-
-
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
 
 def sendQuestions():
     try:
@@ -51,44 +19,41 @@ def sendQuestions():
                                                   user='root',
                                                   password='')
 
-        sql_select_Query = "select QuestionID,sessionID from sessionsdata where userID=2"
-        cursor = mySQLconnection.cursor()
-        cursor.execute(sql_select_Query)
-        records = cursor.fetchall()
-
-        # sql_select_Query2 = "select * from questions where questionID=1"
-        # cursor2 = mySQLconnection.cursor()
-        # cursor2.execute(sql_select_Query2)
-        # records2 = cursor.fetchall()
-
-        # print("SELECT questions.question FROM questions INNER JOIN sessionsdata on sessionsdata.QuestionID=questions.questionID WHERE sessionsdata.userID=2;")
+        sql_select_Query = "select QuestionID from sessionsdata where userID=3"
+        cursor1 = mySQLconnection.cursor()
+        cursor1.execute(sql_select_Query)
+        records = cursor1.fetchall()
 
         for row in records:
             #print(questionSelector.get_recommendations(row[0])[0])
 
             # print("-------------------")
+            # print(row[0])
 
             sql_select_Query2 = "select * from questions where questionID=" + str(
                 questionSelector.get_recommendations(row[0])[0])+""
+            # print(str(questionSelector.get_recommendations(row[0])[0]))
             cursor2 = mySQLconnection.cursor()
             cursor2.execute(sql_select_Query2)
             records2 = cursor2.fetchall()
+            # print(records2)
 
             recQuizList.append(records2)
-            # sentjson = json.dumps({'q_id': row[0][0], 'question': row[0][1]})
 
 
         for row2 in recQuizList:
-            print(row2[0][0])
-            print(row2[0][1])
-            print("----------")
+            # print(row2[0][0])
+            # print(row2[0][1])
+            # print("----------")
+            answerList = row2[0][7].split(',')
+            print(answerList)
 
-            recQuizList2.append({'q_id':row2[0][0], 'question':row2[0][1]})
+            recQuizList2.append({'qs_id': row2[0][0], 'qs_topic': row2[0][9], 'question': row2[0][1], 'options': {'op1': row2[0][2], 'op2': row2[0][3], 'op3': row2[0][4], 'op4': row2[0][5], 'op5': row2[0][6]}, 'answers': answerList,   'difficulty': row2[0][10]})
 
     except Error as e:
         print("Error while connecting to MySQL", e)
 
-    print(recQuizList[0][0][8])
+    # print(recQuizList[0][0][8])
 
     return json.dumps(recQuizList2)
 
