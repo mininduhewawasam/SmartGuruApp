ddc0f3d0e4a83ed22b75b4aa5b8fc13712035ca1
Susith1995 <susithlion@gmail.com>
2019-05-04 23:54:06 +0530

    Update RegisterUsers.py

--- SmartGuruAppServerside/RegisterUsers.py
+++ SmartGuruAppServerside/RegisterUsers.py
@@ -6,3 +6,55 @@ from pandas._libs import json
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+
+try:
+    mySQLconnection = mysql.connector.connect(host='www.remotemysql.com',
+                                              database='u2oI1tyJuT',
+                                              user='u2oI1tyJuT',
+                                             password='joBxFoudcl')
+
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+
+@app.route('/')
+def showSignUp():
+   return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST', 'GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # validate the received values
+        if _name and _email and _password:
+
+            # All Good, let's call MySQL
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message': 'User created successfully !'})
+            else:
+                return json.dumps({'error': str(data[0])})
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__ == '__main__':
+    app.run()
