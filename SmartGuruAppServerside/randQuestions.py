84d4d52aef608276156d7a7fe5a26fc3e3f5f5be
Sandusha Marasinghe <47356467+SandushaM@users.noreply.github.com>
2019-05-04 15:56:08 +0530

    Update randQuestions.py

--- SmartGuruAppServerside/randQuestions.py
+++ SmartGuruAppServerside/randQuestions.py
@@ -14,10 +14,12 @@ def generaterandmQuiz():
                                               user='u2oI1tyJuT',
                                               password='joBxFoudcl')
 
+
     select_topics_query = "select topic from questions group by topic"
     cursor1 = mySQLconnection.cursor()
     cursor1.execute(select_topics_query)
     topics = cursor1.fetchall()
+
 
     for topic in topics:
         topicList.append(topic[0])
@@ -27,10 +29,12 @@ def generaterandmQuiz():
     for i in random.sample(range(len(topicList)), 3):
         random_topics.append(topicList[i])
 
+
     print("------------------------------------")
     print("random_topics")
     print(random_topics)
     print("------------------------------------")
+
 
     for i in range(len(random_topics)):
         select_questions_query = "select * from questions where topic='" + random_topics[i] + "'"
@@ -38,23 +42,9 @@ def generaterandmQuiz():
         cursor2.execute(select_questions_query)
         questions = cursor2.fetchall()
 
+
         for rand_question in questions:
 
-<<<<<<< HEAD
-    count = 1
-    while count <= 10:
-        rand_question = random.choice(records)
-        answerList = []
-        for i in range(7, 11):
-            if rand_question[i] != "":
-                answerList.append(rand_question[i])
-
-        # answerList = rand_question[7].split(',')
-        recQuizList.append({'qs_id': rand_question[0], 'qs_topic': rand_question[13],  'qs_chapter':rand_question[12], 'question': rand_question[1],
-                            'options': {'op1': rand_question[2], 'op2': rand_question[3], 'op3': rand_question[4],
-                                        'op4': rand_question[5], 'op5': rand_question[6]}, 'answers': answerList,
-                            'difficulty': rand_question[10]})
-=======
             answerList = rand_question[7].split(',')
             questionList.append(
                 {'qs_id': rand_question[0], 'qs_topic': rand_question[9], 'qs_chapter': rand_question[8],
@@ -63,10 +53,11 @@ def generaterandmQuiz():
                              'op4': rand_question[5], 'op5': rand_question[6]}, 'answers': answerList,
                  'difficulty': rand_question[10]})
 
+
     if len(questionList) > 10:
         for i in random.sample(range(len(questionList)), 10):
             finalQuiz.append(questionList[i])
->>>>>>> ad44747040264c21a4e403c02b970a62c91f3599
+            
 
     return json.dumps(finalQuiz)
 
