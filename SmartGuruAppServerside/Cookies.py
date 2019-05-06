34f4b5b3e98ecf7ec899130c340155b53c266507
Susith1995 <susithlion@gmail.com>
2019-05-06 08:31:55 +0530

    Update Cookies.py

--- SmartGuruAppServerside/Cookies.py
+++ SmartGuruAppServerside/Cookies.py
@@ -13,6 +13,37 @@ try:
 except Error as e:
     print("Error while connecting to MySQL", e)
 
+
+def login():
+    if request.method == 'POST':
+       session.pop('user', None)
+
+       username=request.form['username']
+
+       sql_select_Query = "SELECT * FROM users WHERE userName='"+username+"'"
+       cursor1 = mySQLconnection.cursor()
+       cursor1.execute(sql_select_Query)
+       records = cursor1.fetchall()
+
+       if request.form['password'] == (records[0][3]):
+           session['user'] = request.form['username']
+           return "USer logged Successfully! \n TODO:Load to homepage"
+
+    return render_template('index.html')
+
+
+def redirect():
+   if g.user:
+     return render_template('protected.html')
+
+   return  render_template('index.html')
+
+def before_request():
+    g.user = None
+    if 'user' in session:
+     g.user = session['user']
+
+
 def getsession():
    if 'user' in session:
      return session['user']
@@ -23,3 +54,4 @@ def getsession():
 def dropsession():
    session.pop('user', None)
    return 'Dropped!'
+
