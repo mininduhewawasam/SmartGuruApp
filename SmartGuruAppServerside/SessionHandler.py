c727cc4bb0208b64c0ae3fddab69ef1c71b913f7
Susith1995 <susithlion@gmail.com>
2019-05-06 08:30:17 +0530

    Update SessionHandler.py

--- SmartGuruAppServerside/SessionHandler.py
+++ SmartGuruAppServerside/SessionHandler.py
@@ -1,57 +1,30 @@
-from flask import Flask,request,session,render_template,g
-import os
+from flask import session
 import mysql.connector
-from mysql.connector import Error
+import json
 
-try:
+
+def user_login(data):
+
     mySQLconnection = mysql.connector.connect(host='www.remotemysql.com',
                                               database='u2oI1tyJuT',
                                               user='u2oI1tyJuT',
-                                             password='joBxFoudcl')
+                                              password='joBxFoudcl')
 
+    user_details = json.loads(data)
 
-except Error as e:
-    print("Error while connecting to MySQL", e)
+    username = user_details.get("username")
+    password = user_details.get("password")
 
+    sql_select_Query = "SELECT * FROM users WHERE userName='" + username + "'"
+    cursor1 = mySQLconnection.cursor()
+    cursor1.execute(sql_select_Query)
+    records = cursor1.fetchall()
 
-def login():
-    if request.method == 'POST':
-       session.pop('user', None)
-
-       username=request.form['username']
-
-       sql_select_Query = "SELECT * FROM users WHERE userName='"+username+"'"
-       cursor1 = mySQLconnection.cursor()
-       cursor1.execute(sql_select_Query)
-       records = cursor1.fetchall()
-
-       if request.form['password'] == (records[0][3]):
-           session['user'] = request.form['username']
-           return "USer logged Successfully! \n TODO:Load to homepage"
-
-    return render_template('index.html')
-
-
-def redirect():
-   if g.user:
-     return render_template('protected.html')
-
-   return  render_template('index.html')
-
-def before_request():
-    g.user = None
-    if 'user' in session:
-     g.user = session['user']
-
-
-def getsession():
-   if 'user' in session:
-     return session['user']
-
-   return  'not logged in'
-
-
-def dropsession():
-   session.pop('user', None)
-   return 'Dropped!'
-
+    if len(records) == 0:
+        return json.dumps({"status": "User Doesn't Exist"})
+    else:
+        if password == (records[0][3]):
+            session['user'] = username
+            return json.dumps({"status": "Access Allowed", "userID": records[0][0], "userType": records[0][4]})
+        else:
+            return json.dumps({"status": "Incorrect Password"})
