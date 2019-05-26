1d8cf4171925369c16bf0191b79c1ff5687be2f9
sanethinduwara <47098118+sanethinduwara@users.noreply.github.com>
2019-05-27 00:58:10 +0530

    Update edit_profile.py

--- SmartGuruAppServerside/edit_profile.py
+++ SmartGuruAppServerside/edit_profile.py
@@ -4,14 +4,14 @@ import mysql.connector
 import json
 
 
-def get_user_details(username):
+def get_user_details(user_id):
 
     mySQLconnection = mysql.connector.connect(host='www.remotemysql.com',
                                               database='u2oI1tyJuT',
                                               user='u2oI1tyJuT',
                                               password='joBxFoudcl')
 
-    sql_select_Query = "SELECT * FROM users WHERE userName='" + username + "'"
+    sql_select_Query = "SELECT * FROM users WHERE userID='" + user_id + "'"
     cursor1 = mySQLconnection.cursor()
     cursor1.execute(sql_select_Query)
     user = cursor1.fetchone()
@@ -20,7 +20,7 @@ def get_user_details(username):
     return json.dumps({"username": user[1], "email": user[2], "password": user[3]})
 
 
-def update_user_details(username, data):
+def update_user_details(user_id, data):
 
     SQLconnection = mysql.connector.connect(host='www.remotemysql.com',
                                               database='u2oI1tyJuT',
@@ -29,8 +29,8 @@ def update_user_details(username, data):
 
     response = json.loads(data)
     cursor = SQLconnection.cursor()
-    sql = "UPDATE users SET userName = %s, userEmail = %s, password = %s WHERE userName = %s"
-    val = (response.get("username"), response.get("email"), response.get("password"), username)
+    sql = "UPDATE users SET userName = %s, userEmail = %s, password = %s WHERE userID = %s"
+    val = (response.get("username"), response.get("email"), response.get("password"), user_id)
     cursor.execute(sql, val)
     SQLconnection.commit()
 
